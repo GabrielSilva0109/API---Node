8be3fcc70b39c90e32e35ddfbf6ecf47c757d862
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-01-22 12:02:26 -0300

    1

---
+++ server.js
@@ -0,0 +1,9 @@
+import app from "./src/App.js"
+
+//Definir a Porta
+const PORT = 3000
+
+//Escutar a porta 
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na endereço http://localhost:${PORT}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 //Importar Express
-const express = require('express')
+import express from 'express'
 
 //Criar uma instancia
 const app = express()
 
-//Definir a Porta
-const port = 3000
-
 //Criar rota Padrão
 //Passar dois parametros "req" "res" = Requisição e Resposta
-app.get('/', (req, res) =>{
-    res.send('Ola mundo !')
+app.get('/', (req, res) => {
+    res.send('Curso Node JS')
 })
 
-//Escutar a porta 
-app.listen(port, () => {
-    console.log(`Servidor rodando na endereço http://localhost:${port}`)
-})
+
+export default app
