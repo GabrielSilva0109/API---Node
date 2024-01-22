a298c578c0926b14b53c5e3077fb3052d3481acd
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-01-22 13:30:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import express from 'express'
 //Criar uma instancia
 const app = express()
 
+//Indica para o express ler o body com json
+app.use(express.json())
+
+//Mock
+const selecoes = [
+    {id: 1, selecao: 'Brasil', grupo: 'G'},
+    {id: 2, selecao: 'Suiça', grupo: 'G'},
+    {id: 3, selecao: 'Servia', grupo: 'G'},
+    {id: 4, selecao: 'Camarões', grupo: 'G'}
+]
+
 //Criar rota Padrão
 //Passar dois parametros "req" "res" = Requisição e Resposta
 app.get('/', (req, res) => {
     res.send('Curso Node JS')
 })
 
+app.get('/selecoes', (request, response) => {
+    response.status(200).send({selecoes})
+})
+
+app.post('/selecoes', (req, res) =>{
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada!')
+})
 
 export default app
