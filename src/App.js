53f3c88da9f7f0088ad62638f76e71ceac107c29
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-01-23 10:04:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ function buscarSelecaoPorId(id){
     return selecoes.filter(selecao => selecao.id == id)
 }
 
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
+
 
 //Criar rota Padrão
 //Passar dois parametros "req" "res" = Requisição e Resposta
@@ -31,7 +35,6 @@ app.get('/selecoes', (request, response) => {
 })
 
 app.get('/selecoes/:id', (req, res) =>{
-    //let index = req.params.id
     res.json(buscarSelecaoPorId(req.params.id))
 })
 
@@ -39,5 +42,19 @@ app.post('/selecoes', (req, res) =>{
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada!')
 })
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send('Seleção excluida!')
+})
+
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo = req.body.grupo
+    res.json(selecoes)
+})
+
 
 export default app
