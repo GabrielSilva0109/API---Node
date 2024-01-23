e9590eca6ceaea44e6ea6b0c88ad648d5597fe9e
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-01-23 11:45:12 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function buscarIndexSelecao(id){
     return selecoes.findIndex(selecao => selecao.id == id)
 }
 
-//Rotas
+//Busca todas as Seleções
 app.get('/selecoes', (req, res) => {
     //response.status(200).send({selecoes})
     const sql = "select * from selecoes;"
@@ -31,13 +31,35 @@ app.get('/selecoes', (req, res) => {
     })
 })
 
+//Busca por ID
 app.get('/selecoes/:id', (req, res) =>{
-    res.json(buscarSelecaoPorId(req.params.id))
+    //res.json(buscarSelecaoPorId(req.params.id))
+    const id = req.params.id
+    const sql = "select * from selecoes where id = ?;"
+    conexao.query(sql, id, (erro, resultado) => {
+        const linha = resultado[0]
+        if(erro){
+            console.log(erro)
+            res.status(404).json({'erro': erro})
+        } else {
+            res.status(200).json(linha)
+        }
+    })
 })
 
 app.post('/selecoes', (req, res) =>{
-    selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada!')
+    //selecoes.push(req.body)
+    //res.status(201).send('Seleção cadastrada!')
+    const selecao = req.body
+    const sql = "insert into selecoes SET ?;"
+    conexao.query(sql, selecao, (erro, resultado) => {
+        if(erro){
+            console.log(erro)
+            res.status(404).json({'erro': erro})
+        } else {
+            res.status(201).json(resultado)
+        }
+    })
 })
 
 app.delete('/selecoes/:id', (req, res) => {
